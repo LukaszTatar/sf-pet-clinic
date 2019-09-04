4aae517823bc7c1c786e9f1754ebf3bc6da1e9db
Lukasz Tatarynowicz <tatarynowicz92@gmail.com>
2019-09-04 20:11:07 +0200

    implement interaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/lukasz/springframework/sfpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package lukasz.springframework.sfpetclinic.services;
+
+import lukasz.springframework.sfpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
